61b65bf623607345b5155f330ec7ea9b84345b30
Aidan Patrick Marias <52266188+Silver0006@users.noreply.github.com>
2023-12-11 12:40:20 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,31 @@
+import keyboard, pystray, threading, random, msvcrt
+from PIL import Image
+from playsound import playsound
+
+image = Image.open(r"D:\Python\Fount-Inscriber\Untitled.jpg")
+
+
+def after_click(icon, query):
+    if str(query) == "Exit":
+        icon.stop()
+        exit()
+
+
+def sounds():
+    while True:
+        if keyboard.read_key() and not keyboard.is_pressed("space") and not keyboard.is_pressed("enter"):
+            ranma = random.randint(1,3)
+            playsound("D:\Python\Fount-Inscriber\Key " + str(ranma) + ".mp3", block=False)
+        elif keyboard.is_pressed("enter"):
+            playsound("D:\Python\Fount-Inscriber\enter.mp3", block=False)
+        elif keyboard.is_pressed("space"):
+            playsound("D:\Python\Fount-Inscriber\space.mp3", block=False)
+        
+            
+
+
+        
+sound_thread = threading.Thread(target=sounds, daemon = True)
+sound_thread.start()
+icon = pystray.Icon("FI", image, "Fount Inscriber", menu=pystray.Menu(pystray.MenuItem("Exit", after_click)))
+icon.run()
